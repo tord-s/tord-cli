2f811aa5be8dae3522fa7c913103aacd10deee05
tord-s <tord@softeland.name>
2021-05-14 20:10:47 +0200

    'jngfjnd_jndjfn'

--- tord/__main__.py
+++ tord/__main__.py
@@ -4,20 +4,23 @@ import os
 # odule import MyClass
 # from .funcmodule import my_function
 def main():
-    cmd = sys.argv[1]
-    message = sys.argv[2:]
-    if cmd == 'push':
-        try:
-            commit_m = "'"
-            for word in message:
-                commit_m += word + "_"
-            commit_m = commit_m[:-1]
-            commit_m += "'"
-            query = "git status && git add . && git commit -m " + commit_m + " && git push"
-            os.system(query)
-        except:
-            print('Failed to push to git')
-    else:
-        print('Invalid command')
+    try:
+        cmd = sys.argv[1]
+        message = sys.argv[2:]
+        if cmd == 'push':
+            try:
+                commit_m = "'"
+                for word in message:
+                    commit_m += word + "_"
+                commit_m = commit_m[:-1]
+                commit_m += "'"
+                query = "git status && git add . && git commit -m " + commit_m + " && git push"
+                os.system(query)
+            except:
+                print('Failed to push to git')
+        else:
+            print('Invalid command')
+    except:
+        print("vaild commands: 'push'")
 if __name__ == '__main__':
     main()
