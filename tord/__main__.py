a571c0e1cfeaa48f7fa0fd89df931d44269e5768
tord-s <tord@softeland.name>
2021-05-14 20:08:26 +0200

    'jngfjnd_jndjfn_'

--- tord/__main__.py
+++ tord/__main__.py
@@ -1,30 +1,23 @@
 import sys
 import os
-import argparse
 # from .classm
 # odule import MyClass
 # from .funcmodule import my_function
 def main():
-    parser = argparse.ArgumentParser(description='Command to do')
-    parser.add_argument('command', type=str, help='available commands')
-    parser.add_argument("-m", "--message", required=False,
-	help="name of the user", nargs='+')
-    args = parser.parse_args()
-    cmd = args.command
+    cmd = sys.argv[1]
+    message = sys.argv[2:]
     if cmd == 'push':
         try:
-            raw = args.message[0].split(' ')
-            message = ""
-            for i in raw:
-                message += '_' + i.strip()
-            print(message)
-            query = "git status && git add . && git commit -m " + message + " && git push"
+            commit_m = "'"
+            for word in message:
+                commit_m += word + " "
+            commit_m = commit_m.strip()
+            commit_m += "'"
+            query = "git status && git add . && git commit -m " + commit_m
+            print(query)
             os.system(query)
         except:
             print('something else')
-        
-
-
     else:
         print('failure')
     # my_function('hello world')
